edb3a78e7a440d5a5c00cf4f6e5d1e9b58ac9421
deepak choudhary <dchoudhary4444@gmail.com>
2020-05-15 20:19:05 +0530

    Updates

---
+++ precache-manifest.3ebf1994f7f15a50876c31498868503a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6c2d67ea202efc3d62e8a0fe2439b9",
+    "url": "/React-in-toDoApp/index.html"
+  },
+  {
+    "revision": "2029f4d3ac9545779641",
+    "url": "/React-in-toDoApp/static/css/main.f4cc1c36.chunk.css"
+  },
+  {
+    "revision": "c79cc2f406aee991cdcf",
+    "url": "/React-in-toDoApp/static/js/2.b6e9e304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-toDoApp/static/js/2.b6e9e304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2029f4d3ac9545779641",
+    "url": "/React-in-toDoApp/static/js/main.204e3eb2.chunk.js"
+  },
+  {
+    "revision": "c68a51c73e3dba2a5435",
+    "url": "/React-in-toDoApp/static/js/runtime-main.def7513b.js"
+  }
+]);
